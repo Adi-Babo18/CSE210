4872ce5fd2633c49825e50af63e56d69f14f9c48
Adi-Babo18 <clarisa.403.18@gmail.com>
2026-09-22 10:44:50 -0600

    Stretch Challenge

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,15 +1,72 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter you first name: ");
-        string first = Console.ReadLine();
+        Console.WriteLine("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
 
-        Console.WriteLine("Enter your last name: ");
-        string last = Console.ReadLine();
+        string letter = "";
+        if (percent >= 95)
+        {
+            letter = "A";
+        }
+        else if (percent >= 90)
+        {
+            letter = "A-";
+        }
+        else if (percent >= 88)
+        {
+            letter = "B+";
+        }
+        else if (percent >= 85)
+        {
+            letter = "B";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B-";
+        }
+        else if (percent >= 78)
+        {
+            letter = "C+";
+        }
+        else if (percent >= 75)
+        {
+            letter = "C";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C-";
+        }
+        else if (percent >= 68)
+        {
+            letter = "D+";
+        }
+        else if (percent >= 65)
+        {
+            letter = "D";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D-";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your Grade on average is: {letter} ");
 
-        Console.WriteLine($"Hello {first} {last}.");
+        if (percent >= 70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time");
+        }
     }
 }
