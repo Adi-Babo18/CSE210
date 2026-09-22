08b1848bf41327115d378ba90b35cc93bbb680ad
Adi-Babo18 <clarisa.403.18@gmail.com>
2026-09-22 10:24:14 -0600

    First and Last Name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Enter you first name: ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("Enter your last name: ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Hello {first} {last}.");
     }
 }
